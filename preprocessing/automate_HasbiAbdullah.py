d34e8f8f4ba189a0509d4fae6f958358d425c140
HasbiA6 <hasbiabdullah75571@gmail.com>
2025-12-30 23:15:15 +0700

    first commit

---
+++ preprocessing/automate_HasbiAbdullah.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+
+# Ambil root directory project
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# Path dataset (AMAN untuk Windows & Linux)
+input_file = os.path.join(BASE_DIR, "dataset_raw", "insurance.csv")
+output_file = os.path.join(
+    BASE_DIR, "preprocessing", "data_preprocessed.csv"
+)
+
+def preprocess():
+    df = pd.read_csv(input_file)
+    df.to_csv(output_file, index=False)
+    print("Preprocessing selesai")
+
+if __name__ == "__main__":
+    preprocess()
